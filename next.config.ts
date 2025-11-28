6fb767fddffb98a53073203067098be700d1943d
Walter Chang <jackyhaha8@gmail.com>
2025-11-29 04:14:45 +0800

    [fix] Update next.config.ts to fix frontend's CORS problem from calling Rindexer graphQL API

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/graphql',
-        destination: process.env.GRAPHQL_API_URL || 'http://localhost:3001/graphql',
-      },
-    ];
-  },
-};
+    async rewrites() {
+        return [
+            {
+                source: "/rindexer-proxy/:path*", // 當前端請求這個路徑...
+                destination: "https://marketplace-indexer.zeabur.app/:path*", // Next.js 會幫你轉發到這裡
+            },
+        ]
+    },
+}
 
-export default nextConfig;
+export default nextConfig
